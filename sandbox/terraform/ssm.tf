50ba3a90e596e9c4e0b2f8f26451d412ed9968cf
markc <mark.conway@microfocus.com>
2022-11-02 13:37:16 +0000

    remove quotes

--- sandbox/terraform/ssm.tf
+++ sandbox/terraform/ssm.tf
@@ -2,5 +2,5 @@
 resource "aws_ssm_parameter" "sandbox_ssm" {
     name = "/AccountID"
     type = "String"
-    value = "{data.aws_caller_identity.current.account_id}"
+    value = {data.aws_caller_identity.current.account_id}
 }
