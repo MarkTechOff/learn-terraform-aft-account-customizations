08429abffd6ff5eae206fb2599a50debb74e8153
markc <mark.conway@microfocus.com>
2022-11-07 17:09:31 +0000

    remove SSM paramter - do this from global

--- production/terraform/s3.tf
+++ production/terraform/s3.tf
@@ -1,6 +1,6 @@
 data "aws_caller_identity" "current" {}
 
-resource "aws_s3_bucket" "sandbox_bucket" {
-  bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
+resource "aws_s3_bucket" "production_bucket" {
+  bucket = "aft-prod-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
--- production/terraform/ssm.tf
+++ production/terraform/ssm.tf
@@ -1,9 +1,3 @@
-# Create an SSM parameter with a caller ID
-resource "aws_ssm_parameter" "sandbox_ssm" {
-    name = "/AccountID"
-    type = "String"
-    value = "${data.aws_caller_identity.current.account_id}"
-}
 
 # Create an SSM paramter indicating this is production
 resource "aws_ssm_parameter" "production_ssm" {
