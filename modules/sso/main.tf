ec082ec5e3e320e72faa9d92f4a2cc1cec365003
MarkTechOff <mark.conway@microfocus.com>
2022-11-28 09:37:10 +0000

    create module for SSO permission sets

---
+++ modules/sso/main.tf
@@ -0,0 +1,53 @@
+variable "account_id" {
+    description = "account id to apply permission set"
+    type = string
+}
+
+variable "group_name" {
+    description = "Display name of group"
+    type = string
+}
+
+variable "permissionset_name" {
+    description = "Name of permission set to apply"
+    type = string
+}
+
+data "aws_ssoadmin_instances" "example" {}
+
+#find the SRE permission set
+data "aws_ssoadmin_permission_set" "sso_permset" {
+  instance_arn = tolist(data.aws_ssoadmin_instances.example.arns)[0]
+  name         = "${var.permissionset_name}"
+}
+
+#find the named group
+data "aws_identitystore_group" "sso_group" {
+  identity_store_id = tolist(data.aws_ssoadmin_instances.example.identity_store_ids)[0]
+
+  filter {
+    attribute_path  = "DisplayName"
+    attribute_value = "${var.group_name}"
+  }
+}
+
+# Create permission set assignment   (group, account, permission set)
+resource "aws_ssoadmin_account_assignment" "sso_assign" {
+  instance_arn       = tolist(data.aws_ssoadmin_instances.example.arns)[0]
+  permission_set_arn = data.aws_ssoadmin_permission_set.sso_permset.arn
+
+  principal_id   = data.aws_identitystore_group.sso_group.group_id
+  principal_type = "GROUP"
+
+  target_id   = "${var.account_id}"
+  target_type = "AWS_ACCOUNT"
+}
+
+output "permission_set_arn" {
+  value = data.aws_ssoadmin_permission_set.sso_permset.arn
+}
+
+output "group_id" {
+  value = data.aws_identitystore_group.sso_group.group_id
+}
+
--- sandbox/terraform/main.tf
+++ sandbox/terraform/main.tf
@@ -7,3 +7,12 @@ resource "aws_budgets_budget" "total_cost" {
   time_period_start = "2022-02-01_00:00"
   time_unit         = "MONTHLY"
 }
+
+
+module "sso_permissions" {
+    source = "../../modules/sso" 
+
+    account_id = "${data.aws_caller_identity.current.account_id}"
+    group_name = "NGLZ_AD_SYNC@microfocusdev.com"
+    permissionset_name = "AWSReadOnlyAccess"
+}
